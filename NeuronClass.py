d385d345da431ac2d3713b673f6e60e3b1e15141
Tyler Hanf <tball401@localhost.localdomain>
2019-06-18 21:00:23 -0400

    Updated Comments

--- NeuronClass.py
+++ NeuronClass.py
@@ -2,18 +2,34 @@ import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
 
-/*
+'''
     Represents a single trainable neuron
-    using tanh function as 
-*/
+    using tanh function as activation function
+'''
+
 class Neuron:
+
+    ''' 
+        weight_count: Number of weights in Neuron
+        
+        Start with random weights to begin the learning process
+            then tune 
+    '''
     def __init__(self, weight_count):
-        random.seed(1)
+
+        #keep random seed at one
+        random.seed(1)          
+        #initialize weights between 0 and 1
         self.weights = initial_weights = 2 * random.random((1,1)) - 1
 
+    '''
+        Returns the derivative with respect to some data point x
+    '''
     def tanh_derivative(self, x):
         return 1 - np.tanh(x) ** 2
 
+    '''
+    '''
     def step(self, x):
         dot_product = np.dot(x, self.weights)
         return np.tanh(dot_product)
